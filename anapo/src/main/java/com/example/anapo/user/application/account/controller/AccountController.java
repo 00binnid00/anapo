396f927196d372ec66cbb92058d064581bbc412b
00binnid00 <kimsn152@gmail.com>
2025-09-19 22:38:06 +0900

    회원가입, 로그인 기능

--- anapo/src/main/java/com/example/anapo/user/application/account/controller/AccountController.java
+++ anapo/src/main/java/com/example/anapo/user/application/account/controller/AccountController.java
@@ -17,6 +17,21 @@ public class AccountController {
     // 회원가입
     @PostMapping("/join")
     public Account joinUser(@RequestBody AccountDto accountDto) {
-        return accountService.join(accountDto);
+        Account account = accountService.join(accountDto);
+        if (account == null) {
+            throw new RuntimeException("이미 존재하는 아이디입니다."); // 실패 메시지 처리
+        }
+        return account;
+    }
+
+
+    // 로그인
+    @PostMapping("/login")
+    public Account loginUser(@RequestBody AccountDto accountDto) {
+        Account account = accountService.login(accountDto);
+        if (account == null) {
+            throw new RuntimeException("아이디 또는 비밀번호가 틀렸습니다."); // 실패 메시지 처리
+        }
+        return account;
     }
 }
--- anapo/src/main/java/com/example/anapo/user/domain/account/repository/AccountRepository.java
+++ anapo/src/main/java/com/example/anapo/user/domain/account/repository/AccountRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository <Account,Long> {
+    Optional<Account> findByUserId(String userId);
 }
